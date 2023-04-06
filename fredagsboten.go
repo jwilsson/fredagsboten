44cafadf8f1dae728ddd97356c5729472e6b0821
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2023-04-06 19:26:52 +0200

    Migrate to AWS SDK v2

--- fredagsboten.go
+++ fredagsboten.go
@@ -22,7 +22,7 @@ func handleRequest(ctx context.Context) (string, error) {
 		return "", nil
 	}
 
-	images, err := getImages(os.Getenv("DYNAMO_TABLE_NAME"))
+	images, err := getImages(os.Getenv("DYNAMO_TABLE_NAME"), ctx)
 	if err != nil {
 		return "", err
 	}
--- image.go
+++ image.go
@@ -1,18 +1,20 @@
 package main
 
 import (
-	"github.com/aws/aws-sdk-go/aws/session"
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/config"
 
 	utils "github.com/jwilsson/go-bot-utils"
 )
 
-func getImages(tableName string) (images []Image, err error) {
-	s, err := session.NewSession()
+func getImages(tableName string, ctx context.Context) (images []Image, err error) {
+	cfg, err := config.LoadDefaultConfig(ctx)
 	if err != nil {
 		return images, err
 	}
 
-	err = utils.GetDynamodbData(s, tableName, &images)
+	err = utils.GetDynamodbData(cfg, ctx, tableName, &images)
 
 	return images, err
 }
