8e9d7f46e102f019bbf371d5400f525e2883e05a
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2025-11-01 13:57:11 +0100

    Migrate to AWS CDK from Serverless

--- README.md
+++ README.md
@@ -8,11 +8,11 @@ Fredagsboten is a Slack bot to automatically let your colleagues know it's Frida
 
 ## Setup
 1. Start by creating a [Slack app and setting up Incoming Webhooks](https://slack.com/intl/en-se/help/articles/115005265063-incoming-webhooks-for-slack).
-2. Configure your [AWS account to work with Serverless](https://serverless.com/framework/docs/providers/aws/guide/credentials/).
-3. Configure a `DYNAMO_TABLE_NAME` environmental variable with the name of your DynamoDB table.
-4. Configure a `SLACK_WEBHOOK_URL` environmental variable with your Slack Webhook URL.
+2. Configure a `DYNAMO_TABLE_NAME` environmental variable with the name of your DynamoDB table.
+3. Configure a `SLACK_WEBHOOK_URL` environmental variable with your Slack Webhook URL.
+4. Deploy using `cdk deploy`.
 5. After deploying the first time, add some images to your DynamoDB table, see the item structure below.
-6. Change any other values in `serverless.yml` to fit your needs.
+6. Change any other values in `cdk.go` to fit your needs.
 7. Profit!
 
 ### Example image item
---
+++ cdk.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"os"
+
+	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsdynamodb"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsevents"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awseventstargets"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
+	"github.com/aws/constructs-go/constructs/v10"
+	"github.com/aws/jsii-runtime-go"
+)
+
+type StackProps struct {
+	awscdk.StackProps
+}
+
+func NewFredagsbotenStack(scope constructs.Construct, id string, props *StackProps) awscdk.Stack {
+	var sprops awscdk.StackProps
+	if props != nil {
+		sprops = props.StackProps
+	}
+
+	stack := awscdk.NewStack(scope, &id, &sprops)
+
+	table := awsdynamodb.NewTable(stack, jsii.String(os.Getenv("DYNAMO_TABLE_NAME")), &awsdynamodb.TableProps{
+		PartitionKey: &awsdynamodb.Attribute{
+			Name: jsii.String("image_url"),
+			Type: awsdynamodb.AttributeType_STRING,
+		},
+		ReadCapacity:  jsii.Number(1),
+		WriteCapacity: jsii.Number(1),
+	})
+
+	function := awslambda.NewFunction(stack, jsii.String("FredagsbotenFunction"), &awslambda.FunctionProps{
+		Architecture: awslambda.Architecture_ARM_64(),
+		Runtime:      awslambda.Runtime_PROVIDED_AL2(),
+		Handler:      jsii.String("bootstrap"),
+		Code:         awslambda.Code_FromAsset(jsii.String("./app/build/"), nil),
+		Environment: &map[string]*string{
+			"DYNAMO_TABLE_NAME": jsii.String(*table.TableName()),
+			"RUN_AT_TIME":       jsii.String("08:00"),
+			"SLACK_WEBHOOK_URL": jsii.String(os.Getenv("SLACK_WEBHOOK_URL")),
+		},
+	})
+
+	table.Grant(function, jsii.String("dynamodb:Scan"))
+
+	awsevents.NewRule(stack, jsii.String("FredagsbotenScheduleRule"), &awsevents.RuleProps{
+		Schedule: awsevents.Schedule_Cron(&awsevents.CronOptions{
+			Minute:  jsii.String("0"),
+			Hour:    jsii.String("*"),
+			WeekDay: jsii.String("FRI"),
+			Month:   jsii.String("*"),
+			Year:    jsii.String("*"),
+		}),
+		Targets: &[]awsevents.IRuleTarget{
+			awseventstargets.NewLambdaFunction(function, nil),
+		},
+	})
+
+	return stack
+}
+
+func main() {
+	defer jsii.Close()
+
+	app := awscdk.NewApp(nil)
+
+	NewFredagsbotenStack(app, "FredagsbotenStack", &StackProps{})
+
+	app.Synth(nil)
+}
